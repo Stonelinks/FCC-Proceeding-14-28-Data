9bce85866020d1b47b540dc494c1db29ea7e71ed
Lucas Doyle <lucas.p.doyle@gmail.com>
2014-08-08 13:12:05 +0900

    better pdf extraction with ocr

--- text-extract.js
+++ text-extract.js
@@ -7,6 +7,15 @@ var path = require('path');
 var async = require('async');
 
 var extract = require('pdf-text-extract');
+var pdf_extract = require('pdf-extract');
+
+var pdf_extract_options = {
+  type: 'ocr', // (required), perform ocr to get the text within the scanned image
+  ocr_flags: [
+    '-psm 1',       // automatically detect page orientation
+    'alphanumeric'  // only output ascii characters
+  ]
+};
 
 var textFolder = db.documentsFolder + '-txt';
 if (fs.existsSync(textFolder)) {
@@ -37,18 +46,40 @@ var extractTextFromDocument = function(id, callback) {
 
     console.log('extracting text from ' + documentPath);
     extract(documentPath, function(err, pages) {
-      if (err) {
-        console.dir(err);
-        _done();
-      }
 
-      fs.ensureDir(path.dirname(documentPathText), function() {
-        fs.writeFile(documentPathText, pages.join('\n\n'), _done);
-      });
+      var _err = function(err) {
+        if (err) {
+          console.dir(err);
+          _done();
+        }
+      };
+      _err(err);
+
+      var _write = function(text) {
+        fs.ensureDir(path.dirname(documentPathText), function() {
+          fs.writeFile(documentPathText, text, _done);
+        });
+      };
+
+      if (pages.length) {
+        _write(pages.join('\n\n'));
+      }
+      else {
+        console.log('trying ocr on ' + documentPath);
+        var processor = pdf_extract(documentPath, pdf_extract_options, _err);
+        processor.on('complete', function(data) {
+          if (data.text_pages.length) {
+            _write(data.text_pages.join('\n\n'));
+          }
+          else {
+            _done();
+          }
+        });
+      }
     });
   }, 200);
 };
 
-async.eachLimit(db.keys(), 12, extractTextFromDocument, function() {
+async.eachLimit(db.keys(), 7, extractTextFromDocument, function() {
   console.log('done');
 });
